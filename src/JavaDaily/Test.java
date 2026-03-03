ea4f502e161d3697f0336267d657e58d6d8e119d
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-04 03:44:18 +0530

    add all

---
+++ src/JavaDaily/Test.java
@@ -0,0 +1,13 @@
+//package JavaDaily;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        int n =6;
+//        if(n & 1){
+//            System.out.print("Even");
+//        }
+//        else {
+//            System.out.print("Odd");
+//        }
+//    }
+//}
