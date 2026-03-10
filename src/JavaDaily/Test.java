b9c066627f44535a23bea3e112449d16e5d09122
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-10 20:45:40 +0530

    is happy number addedd

--- src/JavaDaily/Test.java
+++ src/JavaDaily/Test.java
@@ -4,61 +4,10 @@ package JavaDaily;
 import java.util.*;
 
 
- class Test {
-     public static void main(String[] args) {
+class Test {
+    public static void main(String[] args) {
 
-         String[] strs = {"eat","tea","tan","ate","nat","bat"};
-
-         System.out.println(groupAnagrams(strs));
-
-     }
-
-
-     public static List<List<String>> groupAnagrams(String[] strs) {
-
-         Map<String, List<String>>  map = new HashMap<>();
-
-         for(String st : strs){
-
-            String key = isSorted(st);
-          if(map.containsKey(key)){
-              map.get(key).add(st);
-
-          }
-          else {
-              List<String> li = new ArrayList<>();
-              li.add(st);
-              map.put(key,li);
-          }
-
-
-         }
-
-         return new ArrayList<>(map.values());
-
-
-     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     public static String isSorted(String s){
-         char[] ch = s.toCharArray();
-         Arrays.sort(ch);
-         return new String(ch);
-     }
-
-
+    }
 
 
 }
--- src/JavaDaily/Test2.java
+++ src/JavaDaily/Test2.java
@@ -8,16 +8,5 @@ import java.util.Map;
 public class Test2 {
     public static void main(String[] args) {
 
-        Map<Integer,Integer> map = new HashMap<>();
-
-
-        int arr[] = {1,3,45,6,67};
-        for(int ch : arr){
-            if(map.containsKey(ch)){
-                map.get(ch).ad
-            }
-        }
-
-
     }
 }
