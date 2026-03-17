49ae590c90a2b86245f8aa01553d45f2b62b74e3
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-18 02:27:44 +0530

    mergeintetval addedd

--- src/Leetcode_150/MergeInterval.java
+++ src/Leetcode_150/MergeInterval.java
@@ -1,35 +1,39 @@
 package Leetcode_150;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 
 public class MergeInterval {
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{4,6},{8,10},{15,18}};
+        int[][] intervals = {{1, 3}, {4, 6}, {8, 10}, {15, 18}};
 
-        int n = intervals.length;
+    }
 
-        Arrays.sort(intervals[0]);
-        Arrays.sort(intervals[1]);
+    public static int[][] merge(int[][] intervals) {
 
-//        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < intervals[i].length; j++) {
-                System.out.print(intervals[i][j] + " ");
-            }
-            System.out.println();
+        if(intervals.length<=1){
+            return intervals;
         }
+
+        Arrays.sort(intervals,Comparator.comparing(i->i[0]));
+
+        List<int []> li = new ArrayList<>();
+
+        int ar[] = intervals[0];
+        li.add(ar);
+
+        for(int[] intr : intervals ){
+            if(intr[0] <= ar[1]){
+                ar[1]=Math.max(ar[1],intr[1]);
+            }
+            else {
+                ar=intr;
+                li.add(intr);
+
+            }
+        }
+        return li.toArray(new int[li.size()][]);
 
 
 
     }
-//    public static int[][] merge(int[][] intervals) {
-//
-//
-//
-//
-//    }
 }
