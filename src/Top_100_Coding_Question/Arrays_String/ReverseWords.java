eab6ff8d73bcb31f47bf7555a510f9b3faa8c734
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-03-23 21:49:41 +0530

    Reverse words in a sentence without reversing individual characters

---
+++ src/Top_100_Coding_Question/Arrays_String/ReverseWords.java
@@ -0,0 +1,44 @@
+package Top_100_Coding_Question.Arrays_String;
+
+import java.util.Scanner;
+
+
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(reverseWords(str));
+
+    }
+    public static String reverseWords(String str){
+       StringBuffer sb = new StringBuffer();
+        String st[] = str.split(" ");
+
+
+        for (int i=0;i<st.length;i++){
+
+           if(i!=0){
+               sb.append(" ");
+               sb.append(reverseStr(st[i]));
+           }
+           else {
+               sb.append(reverseStr(st[i]));
+           }
+        }
+        return sb.toString();
+
+
+
+    }
+    public static String reverseStr(String str){
+        String t ="";
+        for (int i=str.length()-1;i>=0;i--){
+            char ch = str.charAt(i);
+            t=t+ch;
+        }
+        return t;
+    }
+
+}
