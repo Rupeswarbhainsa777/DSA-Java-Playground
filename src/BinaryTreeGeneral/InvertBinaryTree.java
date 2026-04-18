e736dfc549b531276ed63fc78942b76684d8a1d2
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-18 13:53:25 +0530

    invertTree add

---
+++ src/BinaryTreeGeneral/InvertBinaryTree.java
@@ -0,0 +1,46 @@
+package BinaryTreeGeneral;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode invertTree1(TreeNode root) {
+        if (root == null) return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right= temp;
+
+            if(node.left!=null){
+                queue.add(node.left);
+            }
+            if(node.right!=null){
+                queue.add(node.right);
+            }
+
+        }
+        return root;
+
+    }
+
+
+    // Recursive  approach
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+        TreeNode right = invertTree2(root.right);
+        TreeNode left = invertTree2(root.left);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
