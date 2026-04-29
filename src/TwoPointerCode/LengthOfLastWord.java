6cf07e4217d0e36482b51bde2c9cdf2f837c83ed
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-04-29 21:32:46 +0530

    LengthOfLastWord this is the problem i solve by three approch

--- src/TwoPointerCode/LengthOfLastWord.java
+++ src/TwoPointerCode/LengthOfLastWord.java
@@ -3,12 +3,28 @@ package TwoPointerCode;
 public class LengthOfLastWord {
     public static void main(String[] args) {
         String str = "luffy is still joyboy";
-        System.out.print(lengthOfLastWord(str));
+        System.out.print(lengthOfLastWord1(str));
     }
-    public static int lengthOfLastWord1(String s){
-        int i=s.length()-1;
+
+    public static int lengthOfLastWord1(String s) {
+        int i = s.length() - 1;
+
+        int count = 0;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+
+            i--;
+
+        }
+
+
+        return count;
 
     }
+
     public static int lengthOfLastWord(String s) {
 
         String str = s.trim();
