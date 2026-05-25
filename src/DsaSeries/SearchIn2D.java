ec6f0ac58ba1352d501f5a078c40c46373b131df
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-05-26 03:12:20 +0530

    Search in 2d arrays done

---
+++ src/DsaSeries/SearchIn2D.java
@@ -0,0 +1,21 @@
+package DsaSeries;
+
+public class SearchIn2D {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+
+    }
+}
