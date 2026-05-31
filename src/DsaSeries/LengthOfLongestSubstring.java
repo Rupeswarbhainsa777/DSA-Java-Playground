439c1ac5baf0c9f49984b0532c513ae43152d232
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-06-01 02:41:54 +0530

    Length of Longest SubString add

---
+++ src/DsaSeries/LengthOfLongestSubstring.java
@@ -0,0 +1,28 @@
+package DsaSeries;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+       String str = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int max = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))){
+                  set.remove(s.charAt(left));
+                  left++;
+            }
+            set.add(s.charAt(right));
+            max = Math.max(max,right-left+1);
+        }
+        return max;
+    }
+}
