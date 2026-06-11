31d830398a9aec727435ff49e875eda5d5d7e84f
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-06-11 23:38:11 +0530

    starting String Compress

--- src/DsaSeries/Strings/StringCompress.java
+++ src/DsaSeries/Strings/StringCompress.java
@@ -1,38 +1,44 @@
-//package DsaSeries.Strings;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class StringCompress {
-//    public static void main(String[] args) {
-//        // {"a","a","b","b","c","c","c"}
-//        char arr[] = {'a','a','b','b','c','c','c'};
+package DsaSeries.Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringCompress {
+    public static void main(String[] args) {
+        // {"a","a","b","b","c","c","c"}
+        char arr[] = {'a','a','b','b','c','c','c'};
 //        System.out.println(compress(arr));
-//    }
-//
-//    public static int compress(char[] chars) {
-//        Map<Character, Integer> map = new HashMap<>();
-//
-//        for (char ch : chars) {
-//            map.put(ch, map.getOrDefault(ch, 0) + 1);
-//        }
-//
-//        int count = 0;
-//        StringBuffer sb = new StringBuffer();
-//        for (Map.Entry<Character, Integer> ent : map.entrySet()) {
-//
-//            if (ent.getValue() > 1) {
-//                sb.append(ent.getKey());
-//                sb.append(ent.getValue());
-//                count = count + 2;
-//            } else {
-//                sb.append(ent.getKey());
-//                count++;
-//
-//            }
-//
-//        }
-//        return count;
-//
-//    }
-//}
+
+        for (int i=0;i<=10;i++){
+             i++;
+            System.out.println(i);
+        }
+    }
+
+    public static int compress(char[] chars) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : chars) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int count = 0;
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<Character, Integer> ent : map.entrySet()) {
+
+            if (ent.getValue() > 1) {
+                sb.append(ent.getKey());
+                sb.append(ent.getValue());
+                count = count + 2;
+            } else {
+                sb.append(ent.getKey());
+                count++;
+
+            }
+
+        }
+        System.out.println(sb.toString());
+        return count;
+
+    }
+}
