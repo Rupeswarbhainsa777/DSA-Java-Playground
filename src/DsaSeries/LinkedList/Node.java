359150c321ec6d708f84b085329a6e20a3057791
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-06-24 00:11:08 +0530

    Start linked list

---
+++ src/DsaSeries/LinkedList/Node.java
@@ -0,0 +1,16 @@
+package DsaSeries.LinkedList;
+
+public class Node {
+
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+
+    }
+
+
+}
---
+++ src/DsaSeries/Raw/Testing.java
@@ -0,0 +1,11 @@
+package DsaSeries.Raw;
+
+public class Testing {
+    public static void main(String[] args) {
+
+        Integer data1 = 12;
+        int data2 = data1;
+        System.out.println(data1);
+        System.out.println(data2);
+    }
+}
