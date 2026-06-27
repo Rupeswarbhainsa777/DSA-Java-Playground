396959443b7d2dc69250890f95dc51641702e8bd
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-06-28 02:13:55 +0530

    Linklist cycle

---
+++ src/DsaSeries/LinkedList/LinkedListCycle.java
@@ -0,0 +1,25 @@
+package DsaSeries.LinkedList;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
