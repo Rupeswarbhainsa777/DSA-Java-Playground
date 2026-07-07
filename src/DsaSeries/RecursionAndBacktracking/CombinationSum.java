83bcd03ac48631dcfc6e9904335d1fd09d42b8a3
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-07-08 03:45:07 +0530

    CombinationSum solved

--- src/DsaSeries/RecursionAndBacktracking/CombinationSum.java
+++ src/DsaSeries/RecursionAndBacktracking/CombinationSum.java
@@ -1,13 +1,39 @@
 package DsaSeries.RecursionAndBacktracking;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CombinationSum {
     public static void main(String[] args) {
 
     }
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-            return new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        helper(candidates,0,new ArrayList<>(),res,target);
+
+        return res;
+    }
+    public  void helper(int arr[], int idx, List<Integer> combination, List<List<Integer>> ans, int t) {
+
+
+        if (idx == arr.length ||t<0 ) return;
+
+
+        if (t == 0) {
+            ans.add(new ArrayList<>(combination));
+            return;
+        }
+
+        combination.add(arr[idx]);
+        // helper(arr, idx + 1, combination, ans, t - arr[idx]);
+        helper(arr, idx, combination, ans, t - arr[idx]);
+        combination.remove(combination.size() - 1);
+        helper(arr, idx + 1, combination, ans, t);
+
+
     }
 }
