df668b3f07c4d1e3cb4de755e9c7ae5d171cfafe
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-07-24 03:26:37 +0530

    best time to buy problem solved

---
+++ src/DsaSeries/DynamicProgramming/BestTimeToBuyStockTwo.java
@@ -0,0 +1,25 @@
+package DsaSeries.DynamicProgramming;
+
+public class BestTimeToBuyStockTwo {
+
+    public static void main(String[] args) {
+
+        int arr[] = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(arr));
+
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int res = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+
+
+        return res;
+    }
+}
