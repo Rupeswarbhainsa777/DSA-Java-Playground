094cc44dd4e4f5b0baf7c2d63e362ec97cd874d0
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-07-28 02:38:39 +0530

    InorderTrav add

--- src/DsaSeries/BinaryTrees/InorderTraversal.java
+++ src/DsaSeries/BinaryTrees/InorderTraversal.java
@@ -1,4 +1,32 @@
 package DsaSeries.BinaryTrees;
 
+import BinaryTreeGeneral.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InorderTraversal {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+
+        helper(root,list);
+
+        return list;
+
+
+    }
+
+    public static void helper(TreeNode node, List<Integer> list) {
+
+        if (node == null) return;
+
+        helper(node.left);
+        list.add(node, val);
+        helper(node.right);
+    }
 }
