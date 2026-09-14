cf2472a97a45b0d8a494534a16ac7020fd3e5ebd
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-09-14 23:33:43 +0530

    is ractnagele

---
+++ src/LeetCode_150/Arrays_String/isRectangle.java
@@ -0,0 +1,17 @@
+package LeetCode_150.Arrays_String;
+
+public class isRectangle {
+
+    public static void main(String[] args) {
+
+    }
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int left = Math.max(rec1[0], rec2[0]);
+        int right = Math.min(rec1[2], rec2[2]);
+
+        int bottom = Math.max(rec1[1], rec2[1]);
+        int top = Math.min(rec1[3], rec2[3]);
+
+        return left < right && bottom < top;
+    }
+}
